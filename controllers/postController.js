b1162e88b065da86998253b124a6cc3d39c2e627
gelbman20 <gelbman20@gmail.com>
2021-07-21 22:42:50 +0300

    Add create method to postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,14 @@
+const Post = require('../models/Post')
+
 exports.viewCreateScreen = function (req, res) {
   res.render('create-post')
+}
+
+exports.create = async function (req, res) {
+  try {
+    const post = await new Post(req.body, req.session.user._id).create()
+    res.send(`Post has been successfully created!, ${post}`)
+  } catch (errors) {
+    res.send(errors)
+  }
 }
