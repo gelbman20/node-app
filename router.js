293e8bc3de69ffb64aff5e571c80e152c060271d
gelbman20 <gelbman20@gmail.com>
2021-07-23 15:55:24 +0300

    Add profile posts route

--- router.js
+++ router.js
@@ -3,11 +3,16 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 
+// User
 router.get('/', userController.home)
 router.post('/login', userController.login)
 router.post('/register', userController.register)
 router.post('/logout', userController.logout)
 
+// Profile
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
+// Post
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
