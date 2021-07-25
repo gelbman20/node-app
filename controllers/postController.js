7cf6db7cb8fba1313f839a5a9fc335f0447602c8
gelbman20 <gelbman20@gmail.com>
2021-07-25 16:13:57 +0300

    Update postController create method; Add postController viewEditScreen method; Add postController edit method

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,7 +7,12 @@ exports.viewCreateScreen = function (req, res) {
 exports.create = async function (req, res) {
   try {
     const post = await new Post(req.body, req.session.user._id).create()
-    res.redirect(`/post/${post._id}`)
+    if (post) {
+      req.flash('success', 'Post successfully created')
+      req.session.save(() => res.redirect(`/post/${post._id}`))
+    } else {
+      res.redirect('404')
+    }
   } catch (errors) {
     res.render('404')
   }
@@ -19,5 +24,32 @@ exports.viewSingle = async function (req, res) {
     res.render('post', { post })
   } catch (errors) {
     res.render('404')
+  }
+}
+
+exports.viewEditScreen = async (req, res) => {
+  try {
+    const post = await Post.getOne(req.params.id, req.visitorId)
+    const { isAuthor } = post
+    if (isAuthor) {
+      res.render('edit-post', { post })
+    } else {
+      res.redirect(`/post/${post._id}`)
+    }
+  } catch (errors) {
+    res.render('404')
+  }
+}
+
+exports.edit = async (req, res) => {
+  try {
+    const post = await new Post(req.body, req.session.user._id, req.params.id).update()
+    if (post) {
+      req.flash('success', 'You successfully changed the post')
+      req.session.save(() => res.redirect(`/post/${req.params.id}/edit`))
+    }
+  } catch (errors) {
+    req.flash('errors', errors)
+    req.session.save(() => res.redirect(`/post/${req.params.id}/edit`))
   }
 }
