edd572e1c59b76f3aad5fa04924102e4862ca029
gelbman20 <gelbman20@gmail.com>
2021-07-31 15:13:42 +0300

    Update search - add open and close actions

--- frontend/modules/search.js
+++ frontend/modules/search.js
@@ -1,5 +1,36 @@
 export default class Search {
   constructor () {
-    console.log('Module Search has been included')
+    this.headerSearchIcon = document.querySelector('[data-header-search-icon]')
+    this.overlay = document.querySelector('[data-search-overlay]')
+    this.closeIcon = document.querySelector('[data-search-close]')
+
+    if (this.headerSearchIcon) {
+      this.init()
+      this.events()
+    }
+  }
+
+  init () {
+    this.overlay.style.transition = '.33s visibility ease-in-out, .33s opacity ease-in-out, .33s transform ease-in-out'
+  }
+
+  events () {
+    this.headerSearchIcon.addEventListener('click', (event) => {
+      event.preventDefault()
+      this.openOverlay()
+    })
+
+    this.closeIcon.addEventListener('click', (event) => {
+      event.preventDefault()
+      this.closeOverlay()
+    })
+  }
+
+  openOverlay () {
+    this.overlay.classList.add('search-overlay--visible')
+  }
+
+  closeOverlay () {
+    this.overlay.classList.remove('search-overlay--visible')
   }
 }
