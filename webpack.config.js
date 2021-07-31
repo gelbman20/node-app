446a7d5a7b4e7f6dab86fa5dd4682505e641efb5
gelbman20 <gelbman20@gmail.com>
2021-07-31 14:45:32 +0300

    Add webpack and frontend build

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './frontend/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public/js/')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
