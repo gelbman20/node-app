46c9bc9fee77bb2f73cebdb4682099945ea26b54
gelbman20 <gelbman20@gmail.com>
2021-08-03 12:58:47 +0300

    Update addFollow action in followController

--- controllers/followController.js
+++ controllers/followController.js
@@ -2,11 +2,11 @@ const Follow = require('../models/Follow')
 
 exports.addFollow = async (req, res) => {
   try {
-    await new Follow(req.params.username, req.visitorId)
+    await new Follow(req.params.username, req.visitorId).create()
     req.flash('success', `Success followed on ${req.params.username}`)
     req.session.save(() => res.redirect(`/profile/${req.params.username}`))
   } catch (errors) {
     req.flash('errors', errors)
-    res.session.save(() => res.render('404'))
+    req.session.save(() => res.redirect('/404'))
   }
 }
