252b62b3c688e828be6aa112ba9a27f2b9e5c615
gelbman20 <gelbman20@gmail.com>
2021-08-03 16:42:42 +0300

    Add removeFollow action to followController

--- controllers/followController.js
+++ controllers/followController.js
@@ -10,3 +10,15 @@ exports.addFollow = async (req, res) => {
     req.session.save(() => res.redirect('/404'))
   }
 }
+
+exports.removeFollow = async (req, res) => {
+  try {
+    const result = await new Follow(req.params.username, req.visitorId).remove()
+    console.log('Result', result)
+    req.flash('success', `Success unfollowed on ${req.params.username}`)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  } catch (errors) {
+    req.flash('errors', errors)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  }
+}
