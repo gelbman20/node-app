216b0bb29a30176a606f162c5510073bf43bc417
gelbman20 <gelbman20@gmail.com>
2021-08-03 11:08:27 +0300

    Add dompurify package to search; Add searchResult html; Add get request;

--- frontend/modules/search.js
+++ frontend/modules/search.js
@@ -1,4 +1,37 @@
 import axios from 'axios';
+import dompurify from 'dompurify'
+
+/**
+ *
+ * @param {Number} number
+ * @return {`<div class="list-group-item active"><strong>Search Results</strong> (${string} items found)</div>`}
+ */
+const activeListItem = (number) => `<div class="list-group-item active"><strong>Search Results</strong> (${number} items found)</div>`
+
+/**
+ *
+ * @param {Object} item
+ * @param {String} [item.title]
+ * @param {ObjectID} [item._id]
+ * @param {Object} [item.author]
+ * @param {Date} [item.createdDate]
+ * @return {`<a class="list-group-item list-group-item-action" href="/post/${string}">
+            <img class="avatar-sm rounded-circle mr-2" src="${*}">
+            <strong class="d-inline-block mr-2">${String}</strong>
+            <span class="text-muted small">by ${string} on ${number}/${number}/${number}</span>
+          </a>`}
+ */
+const listItem = (item) => {
+  return dompurify.sanitize(`<a class="list-group-item list-group-item-action" href="/post/${item._id}">
+            <img class="avatar-sm rounded-circle mr-2" src="${item.author.avatar}" alt="">
+            <strong class="d-inline-block mr-2">${item.title}</strong>
+            <span class="text-muted small">by ${item.author.username} on ${new Date(item.createdDate).getMonth() + 1}/${new Date(item.createdDate).getDay()}/${new Date(item.createdDate).getFullYear()}</span>
+          </a>`)
+}
+
+const generateList = (posts) => {
+  return activeListItem(posts.length) + posts.map(item => listItem(item)).join('')
+}
 
 export default class Search {
   constructor () {
@@ -7,6 +40,7 @@ export default class Search {
     this.closeIcon = document.querySelector('[data-search-close]')
     this.inputField = document.querySelector('#live-search-field')
     this.resultArea = document.querySelector('[data-live-search-results]')
+    this.resultList = this.resultArea.querySelector('[data-live-search-results-list]')
     this.loaderIcon = document.querySelector('[data-search-loader]')
     this.typingWaitTime = null
     this.previousValue = ''
@@ -51,8 +85,9 @@ export default class Search {
     let value = this.inputField.value
 
     if (value != '' && value !== this.previousValue) {
-      clearTimeout(this.typingWaitTime)
       this.showLoaderIcon()
+      this.hideListResult()
+      clearTimeout(this.typingWaitTime)
       this.typingWaitTime = setTimeout(() => this.sendRequest(), 750)
     }
 
@@ -63,10 +98,33 @@ export default class Search {
     this.loaderIcon.classList.add('circle-loader--visible')
   }
 
+  hideLoaderIcon () {
+    this.loaderIcon.classList.remove('circle-loader--visible')
+  }
+
+  showListResult () {
+    this.resultArea.classList.add('live-search-results--visible')
+  }
+
+  hideListResult () {
+    this.resultArea.classList.remove('live-search-results--visible')
+  }
+
   sendRequest () {
     axios.post('/search', { searchTerm: this.inputField.value })
-    .then(() => {
+    .then((response) => {
+      this.hideLoaderIcon()
 
+      const posts = response.data
+      this.resultList.innerHTML = ''
+
+      if (posts && posts.length) {
+        this.resultList.insertAdjacentHTML('afterbegin', generateList(posts))
+      } else {
+        this.resultList.insertAdjacentHTML('afterbegin', '<div class="list-group-item">No search result</div>')
+      }
+
+      this.showListResult()
     })
     .catch((e) => {
       console.log(e)
